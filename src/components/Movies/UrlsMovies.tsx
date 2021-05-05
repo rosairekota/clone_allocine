db0247c6750f0c298449d0cf238d95112c59b2eb
rkota <rosairekota@gmail.com>
2021-05-05 22:24:41 +0100

    feat:recover movies without pagination

--- src/components/Movies/UrlsMovies.tsx
+++ src/components/Movies/UrlsMovies.tsx
@@ -1,13 +1,9 @@
+const UrlsMovies = {
+    urlMoviesRated: 'https://api.themoviedb.org/3/movie/top_rated?api_key=31be5a6f98fca982101536e359cd308e',
+    urlMoviesPopular: 'https://api.themoviedb.org/3/movie/popular?api_key=31be5a6f98fca982101536e359cd308e',
+    urlMoviesLatest: 'https://api.themoviedb.org/3/movie/popular?api_key=31be5a6f98fca982101536e359cd308e',
+    urlMoviesById: 'https://api.themoviedb.org/3/movie/{movie_id}?api_key=31be5a6f98fca982101536e359cd308e',
+    urlImageOfMovies: 'https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=31be5a6f98fca982101536e359cd308e',
+};
 
-
- const UrlsMovies=
-   
-        {
-            urlMoviesRated: "https://api.themoviedb.org/3/movie/top_rated?api_key=31be5a6f98fca982101536e359cd308e",
-            urlMoviesPopular: "https://api.themoviedb.org/3/movie/popular?api_key=31be5a6f98fca982101536e359cd308e",
-            urlMoviesLatest: "https://api.themoviedb.org/3/movie/popular?api_key=31be5a6f98fca982101536e359cd308e"
-        }
-        
-  
- 
 export default UrlsMovies;
